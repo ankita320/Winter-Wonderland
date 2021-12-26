7cf7f1e62921b019cf043d6b385308e24752625f
Ankita Saha <76600603+ankita320@users.noreply.github.com>
2021-12-26 00:29:44 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+var quotes = ['"Where there is a will, there is a way. If there is a chance in a million that you can do something, anything, to keep what you want from ending, do it. Pry the door open or, if need be, wedge your foot in that door and keep it open."', 
+'"Look up at the stars and not down at your feet. Try to make sense of what you see, and wonder about what makes the universe exist. Be curious." ', '"Follow your inner moonlight; don\'t hide the madness."'];
+
+// create the function called by pressing the button
+function nextQuote() {
+  document.getElementById("quote").innerHTML = quotes[numClicks];
+
+
+  if(numClicks == (quotes.length - 1)){
+    numClicks = 0;
+  }
+    numClicks++;
